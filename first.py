6c9b1c02ed0bbb6f96da4b1a52449a11a12e5cea
Zeba1911 <Zeba.Tomar@cloueq.com>
2023-01-05 17:14:10 +0530

    file

--- first.py
+++ first.py
@@ -1,11 +1,5 @@
-number=0
-binary=0
-a=1
-print("Enter any number :")
-n = int(input())
-while n>0:
-    number=n%2
-    binary=binary+number*a
-    a=a*10
-    n=int(n/2)
-print("binary number of ",n,"=",binary)
+
+num1 = 1.5
+num2 = 6.3
+sum = num1 + num2
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
--- second.py
+++ second.py
@@ -1,10 +1,9 @@
-a = input("Enter a binary number: ")
-a = int(a)
-num,i = 0,0
-while a>0:
-    b = a%10
-    c = b*(2**i)
-    a = a//10
-    num = num + c
-    i+=1
-print("Enter number: ",num)
+def maximum(a, b):
+     
+    if a >= b:
+        return a
+    else:
+        return b
+a = 2
+b = 4
+print(maximum(a, b))
